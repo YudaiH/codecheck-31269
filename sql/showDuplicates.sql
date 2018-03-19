56898dfd1209ca7aece62055877b1fd10554642b
DESKTOP-7F06BRL\Hara <y.hara@moegi.waseda.jp>
2018-03-19 13:46:43 +0900

    過去のバージョンに戻す

--- sql/showDuplicates.sql
+++ sql/showDuplicates.sql
@@ -1,6 +1,6 @@
 -- table: users
 -- Show duplicated entries here
 
-SELECT name,password,gender,created_at,deleted_at
-FROM users
-HAVING COUNT(password)>1;
+SELECT users.name,users.password,users.gender,users.created_at,users.deleted_at
+FROM users INNER JOIN users AS users2
+ON users.name=users2.name AND users.password=users2.password AND users.gender=users2.gender AND users.created_at=users2.created_at AND users.deleted_at=users2.deleted_at;

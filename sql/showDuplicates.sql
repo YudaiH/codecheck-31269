e63c0d24d3ebf53cba93c6b299541666e6d356c1
DESKTOP-7F06BRL\Hara <y.hara@moegi.waseda.jp>
2018-03-19 11:55:38 +0900

    重複を見つける

--- sql/showDuplicates.sql
+++ sql/showDuplicates.sql
@@ -1,3 +1,7 @@
 -- table: users
 -- Show duplicated entries here
 
+SELECT name,password,gender,created_at,deleted_at
+FROM users
+GROUP BY name,password,gender,created_at,deleted_at
+HAVING COUNT(*)>1

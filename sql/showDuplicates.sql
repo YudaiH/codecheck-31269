e89394f2943d23a25b298b93ae0e158a46a1366f
DESKTOP-7F06BRL\Hara <y.hara@moegi.waseda.jp>
2018-03-19 14:04:20 +0900

    一つ戻す

--- sql/showDuplicates.sql
+++ sql/showDuplicates.sql
@@ -2,5 +2,4 @@
 -- Show duplicated entries here
 
 SELECT users.name,users.password,users.gender,users.created_at,users.deleted_at
-FROM users LEFT JOIN users AS users2
-ON users.name=users2.name AND users.password=users2.password AND users.gender=users2.gender AND users.created_at=users2.created_at AND users.deleted_at=users2.deleted_at;
+FROM users INNER JOIN users AS users2 ON users.name=users2.name AND users.password=users2.password AND users.gender=users2.gender AND users.created_at=users2.created_at AND users.deleted_at=users2.deleted_at;
